56d938a439f8d3f659c06e62c2a60a86eaafdbf3
SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
2025-01-28 00:20:45 +0100

    Add conditional camera offset based on cursor - Hristov Rework, Part 1 (#31626)
    (cherry picked from commit 5c782d30283d5e3661e1ce6ebc6d972f12671c41)

--- Content.Shared/Wieldable/SharedWieldableSystem.cs
+++ Content.Shared/Wieldable/SharedWieldableSystem.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Content.Shared.Camera;
 using Content.Shared.Examine;
 using Content.Shared.Hands;
 using Content.Shared.Hands.Components;
@@ -7,6 +8,7 @@ using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Inventory.VirtualItem;
 using Content.Shared.Item;
+using Content.Shared.Movement.Components;
 using Content.Shared.Popups;
 using Content.Shared.Timing;
 using Content.Shared.Verbs;
@@ -27,7 +29,7 @@ using Robust.Shared.Audio;
 
 namespace Content.Shared.Wieldable;
 
-public sealed class WieldableSystem : EntitySystem
+public abstract class SharedWieldableSystem : EntitySystem
 {
     [Dependency] private readonly SharedVirtualItemSystem _virtualItemSystem = default!;
     [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
