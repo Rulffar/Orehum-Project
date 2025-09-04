a74d80ee9471f19c48bd75f138d78319206a5491
Andrey <54708336+BadRyuner@users.noreply.github.com>
2025-09-04 18:37:08 +0300

    add jobs and new art effect (#71)
    * add senior jobs
    
    * glimmer go boom

---
+++ Content.Server/_Orehum/Xenoarchaeology/Artifact/XAEGlimmerSystem.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared.Psionics.Glimmer;
+using Content.Shared.Xenoarchaeology.Artifact;
+using Content.Shared.Xenoarchaeology.Artifact.XAE;
+using Robust.Shared.Random;
+
+
+namespace Content.Server._Orehum.Xenoarchaeology.Artifact;
+
+public sealed class XAEGlimmerSystem : BaseXAESystem<XAEGlimmerComponent>
+{
+    [Dependency] private readonly GlimmerSystem _glimmer = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    protected override void OnActivated(Entity<XAEGlimmerComponent> ent, ref XenoArtifactNodeActivatedEvent args)
+    {
+        _glimmer.SetGlimmerOutput((float)_glimmer.GlimmerOutput + _random.NextFloat(ent.Comp.Min, ent.Comp.Max));
+    }
+}
