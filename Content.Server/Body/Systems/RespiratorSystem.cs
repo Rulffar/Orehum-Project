b376a0e1e7451abc65483ae1a0150d65dd577787
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2025-09-10 14:41:27 +0300

    [DeltaV] Port AshWalkers
    From commit https://github.com/DeltaV-Station/Delta-v/commit/85a29fbdf4773dd7b266b335a45db94d8dc4ad76

--- Content.Server/Body/Systems/RespiratorSystem.cs
+++ Content.Server/Body/Systems/RespiratorSystem.cs
@@ -99,7 +99,15 @@ public sealed class RespiratorSystem : EntitySystem
             if (HasComp<RespiratorImmuneComponent>(uid))
                 continue;
 
-            UpdateSaturation(uid, -(float) respirator.UpdateInterval.TotalSeconds, respirator);
+            // Begin DeltaV Code: Addition:
+            var organs = _bodySystem.GetBodyOrganEntityComps<LungComponent>((uid, body));
+            var multiplier = -1f;
+            foreach (var (_, lung, _) in organs)
+            {
+                multiplier *= lung.SaturationLoss;
+            }
+            // End DeltaV Code
+            UpdateSaturation(uid,  multiplier * (float) respirator.UpdateInterval.TotalSeconds, respirator); // DeltaV: use multiplier instead of negating
 
             if (!_mobState.IsIncapacitated(uid) && !HasComp<DebrainedComponent>(uid)) // Shitmed: cannot breathe in crit or when no brain.
             {
